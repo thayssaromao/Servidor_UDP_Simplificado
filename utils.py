fe73c373199321fed6d9daf059260f6990026e79
Matheus Araújo Akiyoshi Loureiro <84807068+MatheusLoureiro2003@users.noreply.github.com>
2025-09-20 21:11:20 +0000

    colocando no utils a função que faltava

--- utils.py
+++ utils.py
@@ -12,6 +12,28 @@ class FileChecker:
             return 0
         return os.path.getsize(self.filename) / (1024 * 1024)
 
+    def read_file(self):
+        if not self.file_exists():
+            print(f"O arquivo '{self.filename}' não existe.")
+            return
+
+        size_mb = self.file_size_mb()
+
+        if size_mb < 1:
+            print(f"O arquivo '{self.filename}' deve ser maior que 1MB")
+            return
+
+        print(f"O arquivo '{self.filename}' tem {size_mb:.2f} MB")
+
+        try:
+            with open(self.filename, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                print("Conteúdo do arquivo:")
+                for line in lines:
+                    print(line.strip())
+        except Exception as e:
+            print(f"ERRO ao ler o arquivo: {str(e)}")
+
 def dividir_arquivo(caminho: str, tamanho_bloco: int):
     """Divide um arquivo em blocos binários."""
     segmentos = []
